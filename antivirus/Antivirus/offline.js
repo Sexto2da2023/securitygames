25bfb92d87be29aec793ad73092d609eb4ffb6f1
Sexto2da2023 <150860206+Sexto2da2023@users.noreply.github.com>
2023-11-30 11:02:19 -0300

    Add files via upload

---
+++ antivirus/Antivirus/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1700049951,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/fondo1.png",
+		"images/yo-sheet0.png",
+		"images/misil-sheet0.png",
+		"images/alien-sheet0.png",
+		"images/alien-sheet1.png",
+		"images/vida1-sheet0.png",
+		"images/fondodegameover.png",
+		"images/misilmalo-sheet0.png",
+		"images/apuntado-sheet0.png",
+		"images/navenodriza-sheet0.png",
+		"images/barralin-sheet0.png",
+		"images/wasd-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"pathfind.js"
+	]
+}
